6366cc1529c2eb731ded70eeaa2eab6dec55acd2
Sanjay Sakthivel <sanjaysakthivel2000@gmail.com>
2024-12-30 13:51:32 -0600

    added planner service

---
+++ planner-service/src/main/java/dayplanner/planner_service/PlannerServiceApplication.java
@@ -0,0 +1,13 @@
+package dayplanner.planner_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerServiceApplication.class, args);
+	}
+
+}
---
+++ planner-service/src/test/java/dayplanner/planner_service/PlannerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dayplanner.planner_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
