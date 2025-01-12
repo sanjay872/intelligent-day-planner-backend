1024764c3433c46b3708560242d162b2d5755124
Sanjay Sakthivel <sanjaysakthivel2000@gmail.com>
2025-01-11 22:42:52 -0600

    add task type controller and updated mapper

--- planner-service/src/main/java/dayplanner/planner_service/dataMapper/PlanDataMapper.java
+++ planner-service/src/main/java/dayplanner/planner_service/dataMapper/PlanDataMapper.java
@@ -4,27 +4,29 @@ import dayplanner.planner_service.dto.PlanDto;
 import dayplanner.planner_service.entity.Plan;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 
-import java.time.Instant;
-import java.time.ZoneId;
+//import org.mapstruct.Named;
+//import java.time.Instant;
+//import java.time.ZoneId;
 
 @Mapper(componentModel = "spring")
 public interface PlanDataMapper {
-//    @Mapping(target = "task.taskName",source = "taskName")
+
 //    @Mapping(target = "createdDate",source = "createdDate",qualifiedByName = "convertStringToDate")
 //    @Mapping(target = "updatedDate",source = "updatedDate",qualifiedByName = "convertStringToDate")
 //    @Mapping(target = "plannedStartDate",source = "plannedStartDate",qualifiedByName = "convertStringToDate")
 //    @Mapping(target = "plannedEndDate",source = "plannedEndDate",qualifiedByName = "convertStringToDate")
 //    @Mapping(target = "notifyDate",source = "notifyDate",qualifiedByName = "convertStringToDate")
+    @Mapping(target = "task.taskName",source = "taskName")
     Plan fromDto(PlanDto planDto);
 
-//    @Mapping(target = "taskName",source = "task.taskName")
+
 //    @Mapping(target = "createdDate",source = "createdDate",qualifiedByName = "convertDateToString")
 //    @Mapping(target = "updatedDate",source = "updatedDate",qualifiedByName = "convertDateToString")
 //    @Mapping(target = "plannedStartDate",source = "plannedStartDate",qualifiedByName = "convertDateToString")
 //    @Mapping(target = "plannedEndDate",source = "plannedEndDate",qualifiedByName = "convertDateToString")
 //    @Mapping(target = "notifyDate",source = "notifyDate",qualifiedByName = "convertDateToString")
+    @Mapping(target = "taskName",source = "task.taskName")
     PlanDto toDto(Plan plan);
 
 //    @Named("convertStringToDate")
